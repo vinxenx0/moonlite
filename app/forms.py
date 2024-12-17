0113d394aca4e26fff75e749c521abd664235ee9
vinxenx0 <151790767+vinxenx0@users.noreply.github.com>
2024-12-17 12:37:46 +0100

    09090 update

--- app/forms.py
+++ app/forms.py
@@ -9,6 +9,21 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 from app.models.user_model import Users
 
+class PaymentForm(FlaskForm):
+    primary_payment_method = SelectField('Método de Pago Primario', choices=[
+        ('IBAN', 'IBAN'), ('SWIFT', 'SWIFT'), ('Bizum', 'Bizum'),
+        ('PayPal', 'PayPal'), ('CreditCard', 'Tarjeta de Crédito')
+    ], validators=[DataRequired()])
+    primary_payment_details = StringField('Detalles del Método de Pago Primario', validators=[DataRequired()])
+    
+    secondary_payment_method = SelectField('Método de Pago Secundario', choices=[
+        ('IBAN', 'IBAN'), ('SWIFT', 'SWIFT'), ('Bizum', 'Bizum'),
+        ('PayPal', 'PayPal'), ('CreditCard', 'Tarjeta de Crédito')
+    ])
+    secondary_payment_details = StringField('Detalles del Método de Pago Secundario')
+
+    submit = SubmitField('Guardar Métodos de Pago')
+
 class NewUserRegistrationForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=4, max=20)])
     email = StringField('Correo electrónico', validators=[DataRequired(), Email()])
--- app/forms/password.py
+++ app/forms/password.py
@@ -19,18 +19,3 @@ class PasswordChangeForm(FlaskForm):
     password = PasswordField('Nueva Contraseña', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirmar Nueva Contraseña', validators=[DataRequired()])
     submit = SubmitField('Cambiar Contraseña')
-
-class PaymentForm(FlaskForm):
-    primary_payment_method = SelectField('Método de Pago Primario', choices=[
-        ('IBAN', 'IBAN'), ('SWIFT', 'SWIFT'), ('Bizum', 'Bizum'),
-        ('PayPal', 'PayPal'), ('CreditCard', 'Tarjeta de Crédito')
-    ], validators=[DataRequired()])
-    primary_payment_details = StringField('Detalles del Método de Pago Primario', validators=[DataRequired()])
-    
-    secondary_payment_method = SelectField('Método de Pago Secundario', choices=[
-        ('IBAN', 'IBAN'), ('SWIFT', 'SWIFT'), ('Bizum', 'Bizum'),
-        ('PayPal', 'PayPal'), ('CreditCard', 'Tarjeta de Crédito')
-    ])
-    secondary_payment_details = StringField('Detalles del Método de Pago Secundario')
-
-    submit = SubmitField('Guardar Métodos de Pago')
